092d5a2975c3fc3f4ec0049b3de12b605be30c3b
Andi Muh. Rizqi <ikiganteng@gegsulbar.com>
2018-07-14 01:42:05 +0700

    Update fftauto.js

--- fftauto.js
+++ fftauto.js
@@ -215,6 +215,8 @@ console.log(chalk`
   —————————————————— [INFORMATION] ————————————————————
 
   [?] {bold.green FFTauto | Using Account/User Target!}
+  [?] {bold.green Gunakan komen.txt untk komen!}
+
 
   ——————————————————  [THANKS TO]  ————————————————————
   [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
